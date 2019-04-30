a8629c5e26013856d32227df06e7df56083f0a78
Daniel Ibrar <daniel.ibrar222@outlook.com>
2019-04-30 20:01:22 +0900

    new scratch

---
+++ ReyukoProject/Helpers/UIHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+
+namespace ReyukoProject.Helpers
+{
+    class UIHelper
+    {
+        public static string App_Title_Color = "#FF2170b6";
+        public static string App_Menu_BackColor = "#FF173a65";
+        public static string App_Menu_ForeColor = "White";
+
+        public static Color HexToColor(string hexColor)
+        {
+            //Remove # if present
+            if (hexColor.IndexOf('#') != -1)
+                hexColor = hexColor.Replace("#", "");
+
+            byte alpha = 0;
+            byte red = 0;
+            byte green = 0;
+            byte blue = 0;
+
+            if (hexColor.Length == 8)
+            {
+                //#AARRGGBB
+                alpha = byte.Parse(hexColor.Substring(0, 2), NumberStyles.AllowHexSpecifier);
+                red = byte.Parse(hexColor.Substring(2, 2), NumberStyles.AllowHexSpecifier);
+                green = byte.Parse(hexColor.Substring(4, 2), NumberStyles.AllowHexSpecifier);
+                blue = byte.Parse(hexColor.Substring(6, 2), NumberStyles.AllowHexSpecifier);
+            }
+
+            return Color.FromArgb(alpha, red, green, blue);
+        }
+    }
+}
---
+++ ReyukoProject/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Windows.UI.Xaml.Controls;
+using ReyukoProject.Pages.Dashboard_Page;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ReyukoProject
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        public Frame AppFrame { get { return this.Frame; } }
+
+        // Properties
+        public readonly string DashboardMenuItemName = "Dashboard";
+        public readonly string SalesItemName = "Sales";
+        public readonly string PurchasingMenuItemName = "Purchasing";
+        public readonly string ProductMenuItemName = "Product";
+        public readonly string InventoryMenuItemName = "Inventory";
+        public readonly string PaymentMenuItemName = "Payment";
+        public readonly string AccountingMenuItemName = "Accounting";
+        public readonly string ContactMenuItemName = "Contact";
+        public readonly string ReportingMenuItemName = "Reporting";
+        public readonly string DocumentMenuItemName = "Document";
+        public readonly string CalendarMenuItemName = "Calendar";
+        public readonly string POSMenuItemName = "POS";
+        public readonly string ServicesMenuItemName = "Services";
+        public readonly string SettingsMenuItemName = "Settings";
+        
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Loaded += (sender, args) =>
+            {
+                NavView.SelectedItem = DashBoardMenuItem;
+            };
+        }
+        private void NavigationView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            var label = args.InvokedItem as string;
+            if (label == SalesItemName)
+                AppFrame.Navigate(typeof(Dashboard_Page));
+            
+        }
+            private void NavView_BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
+        {
+            if (AppFrame.CanGoBack)
+            {
+                AppFrame.GoBack();
+            }
+        }
+    }
+}
